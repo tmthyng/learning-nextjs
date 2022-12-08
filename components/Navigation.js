86473d300f24c183498c5c9c87ae4e755f9b9730
Timothy Ng <contact.tim.ng@gmail.com>
2022-12-09 11:00:17 +1300

    Tweaks to the Navigation

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,9 +1,13 @@
+import Link from 'next/link';
 import styles from '../styles/Navigation.module.css'
 
 const Navigation = () => {
   return (
     <nav className={ styles.navigation }>
-      <h1>Matrix</h1>
+      <Link href='/'>
+        <h1>Matrix</h1>
+      </Link>
+
       <button>Create New</button>
     </nav>
   );
