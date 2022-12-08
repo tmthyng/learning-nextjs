1f023ab93dda444e079093b6f6d7e18772614664
Timothy Ng <contact.tim.ng@gmail.com>
2022-12-09 11:00:27 +1300

    Home page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
+import styles from '../styles/Home.module.css'
+
 export default function Home() {
   return (
-    <div>
-      <h1>hello, world&#33;</h1>
+    <div className={ styles.container }>
+      <h1>
+        Welcome to
+        <br />
+        the Matrix&#33;
+      </h1>
+      
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium quo, nostrum quam, ipsam blanditiis deserunt saepe, non nemo tempore officiis numquam ea? Quia necessitatibus ab laborum eum saepe, voluptatibus eligendi.
       </p>
+
+      <button>View subscriptions</button>
     </div>
   )
 }
