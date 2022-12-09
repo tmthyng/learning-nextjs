b71d8152bb83c69dd82c19ff33c4340c19cbd62d
Timothy Ng <contact.tim.ng@gmail.com>
2022-12-09 14:40:04 +1300

    Global metadata

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
+import Head from 'next/head'
 import Layout from '../components/Layout'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>Welcome to the Matrix&#33;</title>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
 
